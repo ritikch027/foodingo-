051c5610af21391654f944583ea50a8cf2fc23ab
Ritik <ritikchauhan5446@gmail.com>
2025-07-03 18:54:06 +0530

    index.js updated

--- index.js
+++ index.js
@@ -37,5 +37,7 @@ app.get("/", (req, res) => {
 
 // Start the server
 app.listen(PORT, () => {
+  console.log("🟢 Backend redeployed successfully");
+
   console.log(`Server is running on port ${PORT}`);
 });
